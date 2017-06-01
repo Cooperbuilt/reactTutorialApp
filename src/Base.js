0838e9bdcc342133c39ab7c9b741c48f51eeb74e
Evan Cooper <evanncooper@gmail.com>
2017-06-01 08:53:21 -0400

    Full Commit from off-line Build

---
+++ src/Base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCG1jkL2z4iKay-IzzJs6BEgVFt-AecZYs",
+    authDomain: "catch-of-the-day-evan-cooper.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-evan-cooper.firebaseio.com",
+});
+
+export default base;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+
+class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault();
+		
+		const storeID = this.storeInput.value;
+
+		this.context.router.transitionTo(`/store/${storeID}`);
+	}
+
+	render() {
+		return(
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+				<h2>Please Enter a Store</h2>
+				<input 
+					type="text" 
+					required placeholder="Store Name" 
+					defaultValue={getFunName()} 
+					ref={(input) => {this.storeInput = input}} 
+				/>
+				<button type="submit">Visit Store -></button>	
+			</form>
+		)
+	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
+export default StorePicker;
